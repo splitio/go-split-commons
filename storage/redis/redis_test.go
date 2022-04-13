e1ea83f9eaa02b42c2a1674a0037889bffb4a783
Martin Redolatti <martin.redolatti@split.io>
2022-04-13 16:45:07 -0300

    do not panic on failed redis instantiation

---
+++ storage/redis/redis_test.go
@@ -0,0 +1,24 @@
+package redis
+
+import (
+	"errors"
+	"net"
+	"testing"
+
+	"github.com/splitio/go-split-commons/v4/conf"
+	"github.com/splitio/go-toolkit/v5/logging"
+)
+
+func TestRedisConnection(t *testing.T) {
+
+	client, err := NewRedisClient(&conf.RedisConfig{Host: "localhost", Port: 7658}, logging.NewLogger(nil))
+
+	if client != nil {
+		t.Error("client should be nil")
+	}
+
+	var opErr *net.OpError
+	if !errors.As(err, &opErr) {
+		t.Errorf("expected a network error. Got: %T", err)
+	}
+}
