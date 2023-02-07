67d23ccfefa6ac9f82ce3a73c791bb985df022c4
Nadia Mayor <nadia.mayor@split.io>
2023-02-07 11:42:58 -0300

    [SDKS-6471] Add sha and synchronization for splits

--- service/local/splitFetcher.go
+++ service/local/splitFetcher.go
@@ -246,6 +246,28 @@ func (f *FileSplitFetcher) parseSplitsJson(data string) *dtos.SplitChangesDTO {
 	return &splitChangesDto
 }
 
+func (s *FileSplitFetcher) processSplitJson(data string, changeNumber int64) *dtos.SplitChangesDTO {
+	var splitChangesDto dtos.SplitChangesDTO
+	splitChangesDto.Since = changeNumber
+	splitChangesDto.Till = changeNumber
+	splitChange := s.parseSplitsJson(data)
+	splitsJson, err := json.Marshal(splitChange.Splits)
+	if err != nil {
+		s.logger.Error(fmt.Sprintf("error: %v", err))
+		return &splitChangesDto
+	}
+	currH := sha1.New()
+	currH.Write(splitsJson)
+	currSum := currH.Sum(nil)
+	if bytes.Equal(currSum, s.lastHash) || splitChange.Till < changeNumber {
+		return &splitChangesDto
+	}
+	s.lastHash = currSum
+	splitChange.Since = splitChange.Till
+	splitChange.Till++
+	return splitChange
+}
+
 // Fetch parses the file and returns the appropriate structures
 func (s *FileSplitFetcher) Fetch(changeNumber int64, _ *service.FetchOptions) (*dtos.SplitChangesDTO, error) {
 	fileContents, err := ioutil.ReadFile(s.splitFile)
@@ -261,8 +283,7 @@ func (s *FileSplitFetcher) Fetch(changeNumber int64, _ *service.FetchOptions) (*
 	case SplitFileFormatYAML:
 		splits = s.parseSplitsYAML(data)
 	case SplitFileFormatJSON:
-		splitChange := s.parseSplitsJson(data)
-		splitChange.Since = splitChange.Till
+		splitChange := s.processSplitJson(data, changeNumber)
 		return splitChange, nil
 	default:
 		return nil, fmt.Errorf("unsupported file format")
@@ -273,7 +294,7 @@ func (s *FileSplitFetcher) Fetch(changeNumber int64, _ *service.FetchOptions) (*
 
 	// Get the SHA1 sum of the raw contents of the file, and compare it to the last one seen
 	// if it's equal, nothing has changed, return since == till
-	// otherwise, something changed, return till = since + 1
+	// otherwise, something changed, return till = since + 1  s
 	currH := sha1.New()
 	currH.Write(fileContents)
 	currSum := currH.Sum(nil)
