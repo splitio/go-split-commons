ed93d35151e2fc3df2b720d6ae5aa4c6f0ac76be
Lucas Echeverz <lucas.echeverz@split.io>
2023-10-27 11:08:25 -0300

    flagsets telemetry

--- telemetry/constants_test.go
+++ telemetry/constants_test.go
@@ -25,6 +25,26 @@ func TestMethodMapping(t *testing.T) {
 			t.Error("expented `treatmentsWithConfig`. Got: ", v)
 		}
 	}
+	for _, method := range []string{"treatmentsByFlagSet", "getTreatmentsByFlagSet", "get_treatments_by_flag_set", "TreatmentsByFlagSet"} {
+		if v, ok := ParseMethodFromRedisHash(method); !ok || v != TreatmentsByFlagSet {
+			t.Error("expected `treatmentsByFlagSet`. Got: ", v)
+		}
+	}
+	for _, method := range []string{"treatmentsByFlagSets", "getTreatmentsByFlagSets", "get_treatments_by_flag_sets", "TreatmentsByFlagSets"} {
+		if v, ok := ParseMethodFromRedisHash(method); !ok || v != TreatmentsByFlagSets {
+			t.Error("expected `treatmentsByFlagSets`. Got: ", v)
+		}
+	}
+	for _, method := range []string{"treatmentsWithConfigByFlagSet", "getTreatmentsWithConfigByFlagSet", "get_treatments_with_config_by_flag_set", "TreatmentsWithConfigByFlagSet"} {
+		if v, ok := ParseMethodFromRedisHash(method); !ok || v != TreatmentsWithConfigByFlagSet {
+			t.Error("expected `treatmentWithConfigByFlagSet`. Got: ", v)
+		}
+	}
+	for _, method := range []string{"treatmentsWithConfigByFlagSets", "getTreatmentsWithConfigByFlagSets", "get_treatments_with_config_by_flag_sets", "TreatmentsWithConfigByFlagSets"} {
+		if v, ok := ParseMethodFromRedisHash(method); !ok || v != TreatmentsWithConfigByFlagSets {
+			t.Error("expected `treatmentWithConfigByFlagSets`. Got: ", v)
+		}
+	}
 	for _, method := range []string{"track", "Track"} {
 		if v, ok := ParseMethodFromRedisHash(method); !ok || v != Track {
 			t.Error("expented `track`. Got: ", method)
