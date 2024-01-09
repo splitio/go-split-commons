a6aff56069aef98056c0b3b84841581e48f5932d
Mauro Antonio Sanz <mauro.sanz@split.io>
2024-01-09 17:08:18 -0300

    pr feedback

--- storage/redis/splits.go
+++ storage/redis/splits.go
@@ -439,29 +439,33 @@ func (r *SplitStorage) GetNamesByFlagSets(sets []string) map[string][]string {
 }
 
 func (r *SplitStorage) getAllSplitKeys() ([]string, error) {
-	if !r.client.ClusterMode() {
-		var cursor uint64
-		featureFlagNames := make([]string, 0)
-		for {
-			keys, rCursor, err := r.client.Scan(cursor, strings.Replace(KeySplit, "{split}", "*", 1), r.scanCount)
-			if err != nil {
-				return nil, err
-			}
-
-			cursor = rCursor
-			toRemove := strings.Replace(KeySplit, "{split}", "*", 1) // Create a string with all the prefix to remove
-			for _, key := range keys {
-				featureFlagNames = append(featureFlagNames, strings.Replace(key, toRemove, "", 1)) // Extract flag set name from key
-			}
-
-			if cursor == 0 {
-				break
-			}
-		}
-
-		return featureFlagNames, nil
+	if r.client.ClusterMode() {
+		return r.splitKeysClusterMode()
 	}
 
+	var cursor uint64
+	featureFlagNames := make([]string, 0)
+	for {
+		keys, rCursor, err := r.client.Scan(cursor, strings.Replace(KeySplit, "{split}", "*", 1), r.scanCount)
+		if err != nil {
+			return nil, err
+		}
+
+		cursor = rCursor
+		toRemove := strings.Replace(KeySplit, "{split}", "*", 1) // Create a string with all the prefix to remove
+		for _, key := range keys {
+			featureFlagNames = append(featureFlagNames, strings.Replace(key, toRemove, "", 1)) // Extract flag set name from key
+		}
+
+		if cursor == 0 {
+			break
+		}
+	}
+
+	return featureFlagNames, nil
+}
+
+func (r *SplitStorage) splitKeysClusterMode() ([]string, error) {
 	// the hashtag is bundled in the prefix, so it will automatically be added,
 	// and we'll get the slot where all the redis keys are bound
 	slot, err := r.client.ClusterSlotForKey("__DUMMY__")
