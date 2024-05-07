1a170c7840fde2c6e3555d4c212868c8c39f1b25
Matias Melograno <matias.melograno@split.io>
2024-05-07 11:37:27 -0300

    added err var

--- engine/grammar/matchers/matchers.go
+++ engine/grammar/matchers/matchers.go
@@ -10,6 +10,8 @@ import (
 	"github.com/splitio/go-toolkit/v5/logging"
 )
 
+var ErrInvalidGTOESemver = errors.New("semver is required for GREATER_THAN_OR_EQUAL_TO_SEMVER matcher type")
+
 const (
 	// MatcherTypeAllKeys string value
 	MatcherTypeAllKeys = "ALL_KEYS"
@@ -349,7 +351,7 @@ func BuildMatcher(dto *dtos.MatcherDTO, ctx *injection.Context, logger logging.L
 
 	case MatcherTypeGreaterThanOrEqualToSemver:
 		if dto.String == nil {
-			return nil, errors.New("Semver is required for GREATER_THAN_OR_EQUAL_TO_SEMVER matcher type")
+			return nil, ErrInvalidGTOESemver
 		}
 		logger.Debug(fmt.Sprintf(
 			"Building GreaterThanOrEqualToSemverMatcher with negate=%t, semver=%s, attributeName=%v",
