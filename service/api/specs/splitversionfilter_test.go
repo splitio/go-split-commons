71c70fa2eb1d90ebd380d8af275b153756f5bf53
Mauro Antonio Sanz <mauro.sanz@split.io>
2024-07-31 16:41:03 -0300

    adding test

--- service/api/specs/splitversionfilter_test.go
+++ service/api/specs/splitversionfilter_test.go
@@ -6,6 +6,28 @@ import (
 	"github.com/splitio/go-split-commons/v6/engine/grammar/matchers"
 )
 
+func Test_ParseAndValidate(t *testing.T) {
+	res, err := ParseAndValidate("")
+	if err != nil || res != FLAG_V1_0 {
+		t.Error("It should be 1.1")
+	}
+
+	res, err = ParseAndValidate("1.1")
+	if err != nil || res != FLAG_V1_1 {
+		t.Error("It should be 1.1")
+	}
+
+	res, err = ParseAndValidate("1.2")
+	if err != nil || res != FLAG_V1_2 {
+		t.Error("It should be 1.2")
+	}
+
+	res, err = ParseAndValidate("2.3")
+	if err == nil || res != "" {
+		t.Error("Should be a unsupported version")
+	}
+}
+
 func Test_splitVersionFilter(t *testing.T) {
 	shouldFilter := ShouldFilter(matchers.MatcherTypeBetweenSemver, FLAG_V1_0)
 	if !shouldFilter {
