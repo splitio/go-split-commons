9b24ba43e60a30dcb65f72051caf8aba683704b0
Mauro Antonio Sanz <mauro.sanz@split.io>
2024-08-02 16:53:40 -0300

    removed old code

--- service/api/specs/splitversionfilter.go
+++ service/api/specs/splitversionfilter.go
@@ -4,15 +4,6 @@ import (
 	"github.com/splitio/go-split-commons/v6/engine/grammar/matchers"
 )
 
-var V1_1 = map[string]bool{matchers.MatcherInLargeSegment: true}
-var V1_0 = mergeMaps(map[string]bool{
-	matchers.MatcherEqualToSemver:                  true,
-	matchers.MatcherTypeLessThanOrEqualToSemver:    true,
-	matchers.MatcherTypeGreaterThanOrEqualToSemver: true,
-	matchers.MatcherTypeBetweenSemver:              true,
-	matchers.MatcherTypeInListSemver:               true,
-}, V1_1)
-
 type SplitVersionFilter struct {
 	v1_0 map[string]bool
 	v1_1 map[string]bool
@@ -26,7 +17,7 @@ func NewSplitVersionFilter() SplitVersionFilter {
 		matchers.MatcherTypeGreaterThanOrEqualToSemver: true,
 		matchers.MatcherTypeBetweenSemver:              true,
 		matchers.MatcherTypeInListSemver:               true,
-	}, V1_1)
+	}, v1_1)
 
 	return SplitVersionFilter{
 		v1_0: v1_0,
@@ -37,9 +28,9 @@ func NewSplitVersionFilter() SplitVersionFilter {
 func (f *SplitVersionFilter) ShouldFilter(matcher string, apiVersion string) bool {
 	switch apiVersion {
 	case FLAG_V1_1:
-		return V1_1[matcher]
+		return f.v1_1[matcher]
 	case FLAG_V1_0:
-		return V1_0[matcher]
+		return f.v1_0[matcher]
 	}
 
 	return false
