eee0249cc47aa900032c2b69fc9038832384d1ba
Mauro Antonio Sanz <mauro.sanz@split.io>
2024-10-29 16:07:23 -0300

    adding large segment storage in memory implementation

---
+++ storage/inmemory/mutexmap/large_segments.go
@@ -0,0 +1,67 @@
+package mutexmap
+
+import (
+	"sort"
+	"sync"
+
+	"github.com/splitio/go-toolkit/v5/logging"
+)
+
+// LargeSegmentsStorage defines the interface for a per-user large segments storage
+type LargeSegmentsStorage interface {
+	Count() int
+	LargeSegmentsForUser(userKey string) []string
+	Update(lsName string, userKeys []string)
+}
+
+// LargeSegmentsStorageImpl implements the LargeSegmentsStorage interface
+type LargeSegmentsStorageImpl struct {
+	largeSegments map[string][]string
+	mutex         *sync.RWMutex
+	logger        logging.LoggerInterface
+}
+
+// NewLargeSegmentsStorage constructs a new LargeSegments cache
+func NewLargeSegmentsStorage(logger logging.LoggerInterface) *LargeSegmentsStorageImpl {
+	return &LargeSegmentsStorageImpl{
+		largeSegments: make(map[string][]string),
+		mutex:         &sync.RWMutex{},
+		logger:        logger,
+	}
+}
+
+// Count retuns the amount of Large Segments
+func (s *LargeSegmentsStorageImpl) Count() int {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return len(s.largeSegments)
+}
+
+// SegmentsForUser returns the list of segments a certain user belongs to
+func (s *LargeSegmentsStorageImpl) LargeSegmentsForUser(userKey string) []string {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	toReturn := make([]string, 0, len(s.largeSegments))
+	for lsName, data := range s.largeSegments {
+		i := sort.Search(len(data), func(i int) bool {
+			return data[i] >= userKey
+		})
+
+		if i < len(data) && data[i] == userKey {
+			toReturn = append(toReturn, lsName)
+		}
+	}
+
+	return toReturn
+}
+
+// Update adds and remove keys to segments
+func (s *LargeSegmentsStorageImpl) Update(lsName string, userKeys []string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.largeSegments[lsName] = userKeys
+}
+
+var _ LargeSegmentsStorage = (*LargeSegmentsStorageImpl)(nil)
---
+++ storage/inmemory/mutexmap/large_segments_test.go
@@ -0,0 +1,67 @@
+package mutexmap
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+
+	"github.com/splitio/go-toolkit/v5/logging"
+)
+
+func sortedKeys(prefix string, count int, shared *string) []string {
+	keys := make([]string, 0, count)
+	for i := 0; i < count; i++ {
+		keys = append(keys, fmt.Sprintf("%s-user-id-%d", prefix, i))
+	}
+
+	if shared != nil {
+		keys = append(keys, *shared)
+	}
+
+	sort.Strings(keys)
+	return keys
+}
+
+func TestLargeSegmentStorage(t *testing.T) {
+	storage := NewLargeSegmentsStorage(logging.NewLogger(nil))
+
+	keys1 := sortedKeys("ls1", 10000, nil)
+	storage.Update("ls_test_1", keys1)
+
+	sharedKey := &keys1[5000]
+	keys2 := sortedKeys("ls2", 20000, sharedKey)
+	storage.Update("ls_test_2", keys2)
+
+	keys3 := sortedKeys("ls3", 30000, sharedKey)
+	storage.Update("ls_test_3", keys3)
+
+	if storage.Count() != 3 {
+		t.Error("Count should be 3. Actual: ", storage.Count())
+	}
+
+	result := storage.LargeSegmentsForUser(*sharedKey)
+	sort.Strings(result)
+	if result[0] != "ls_test_1" || result[1] != "ls_test_2" || result[2] != "ls_test_3" {
+		t.Error("Count should be 3. Actual: ", result)
+	}
+
+	result = storage.LargeSegmentsForUser(keys1[100])
+	if len(result) != 1 || result[0] != "ls_test_1" {
+		t.Error("Count should be 1. Actual: ", result)
+	}
+
+	result = storage.LargeSegmentsForUser(keys2[100])
+	if len(result) != 1 || result[0] != "ls_test_2" {
+		t.Error("Count should be 1. Actual: ", result)
+	}
+
+	result = storage.LargeSegmentsForUser(keys3[100])
+	if len(result) != 1 || result[0] != "ls_test_3" {
+		t.Error("Count should be 1. Actual: ", result)
+	}
+
+	result = storage.LargeSegmentsForUser("mauro-test")
+	if len(result) != 0 {
+		t.Error("Count should be empty. Actual: ", result)
+	}
+}
