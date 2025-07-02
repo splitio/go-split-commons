b4ad9b5f71adaefa5df60addb2b0ee7aca820fa7
Nadia Mayor <nadia.mayor@split.io>
2025-07-02 16:34:26 -0300

    Update dtos

--- dtos/rulebasedsegment.go
+++ dtos/rulebasedsegment.go
@@ -2,9 +2,9 @@ package dtos
 
 // Excluded segment types
 const (
-	TypeStandard = "standard";
-  	TypeRuleBased = "rule-based";
-  	TypeLarge = "large";
+	TypeStandard  = "standard"
+	TypeRuleBased = "rule-based"
+	TypeLarge     = "large"
 )
 
 // RuleBasedSegment
@@ -12,9 +12,8 @@ type RuleBasedSegment struct {
 	Name            string
 	TrafficTypeName string
 	ChangeNumber    int64
-	Status          Status
-	Conditions      []ConditionDTO
-	
+	Status          string
+	Conditions      []RuleBasedConditionDTO
 }
 
 type Excluded struct {
@@ -28,22 +27,7 @@ type ExcludedSegment struct {
 }
 
 // ConditionDTO structure to map a Condition fetched from JSON message.
-type ConditionDTO struct {
-	ConditionType string         
+type RuleBasedConditionDTO struct {
+	ConditionType string
 	MatcherGroup  MatcherGroupDTO
-}
-
-// RFD struct
-type RFD struct {
-	Data   Data   `json:"d"`
-	Params Params `json:"p"`
-}
-
-// RuleBasedSegmentRFDResponseDTO
-type RuleBasedSegmentRFDResponseDTO struct {
-	Name             string `json:"n"`
-	NotificationType string `json:"t"`
-	RFD              *RFD   `json:"rfd,omitempty"`
-	SpecVersion      string `json:"v"`
-	ChangeNumber     int64  `json:"cn"`
 }
