abc755695ce2532c5578bbf533ccf2e5bc6d2d4e
Nadia Mayor <nadia.mayor@split.io>
2025-07-17 17:18:56 -0300

    [FME-4342] Add spec version

--- service/api/specs/specversion.go
+++ service/api/specs/specversion.go
@@ -6,6 +6,7 @@ const (
 	FLAG_V1_0 = "1.0"
 	FLAG_V1_1 = "1.1"
 	FLAG_V1_2 = "1.2"
+	FLAG_V1_3 = "1.3"
 )
 
 // Match returns the spec version if it is valid, otherwise it returns nil
@@ -17,7 +18,10 @@ func Match(version string) *string {
 		return &version
 	case FLAG_V1_2:
 		return &version
+	case FLAG_V1_3:
+		return &version
 	}
+
 	return nil
 }
 
